5754414ca30814c656dc5a7cf8d181f96ab6d457
maxschommer <maximilian.schommer@students.olin.edu>
2017-05-01 10:49:21 -0400

    Update update_plot.m

--- update_plot.m
+++ update_plot.m
@@ -1,5 +1,6 @@
 function[f] = update_plot(A_t)
-[x, y] = simulate_Cat(-1, 1, 0, .5, A_t, 4);
+%Inputs are: x1, x2, y1, y2, Segments, Initial Length
+[x, y] = simulate_Cat(-1, 1, 0, .5, A_t, 4); 
 f = plot(x, y);
 axis([-1, 1, -2, 1])
 grid on
