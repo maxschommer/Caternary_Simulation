203b5d0f1dddae5024baa5bfea1ea05ab2653a65
maxschommer <maximilian.schommer@students.olin.edu>
2017-05-04 12:46:02 -0400

    Update change_plot.m

--- change_plot.m
+++ change_plot.m
@@ -2,5 +2,5 @@ function change_plot(objHandle, ~)
 slider_value = get(objHandle, 'Value');
 new_A_t = slider_value;
 update_plot(new_A_t);
-pause(40000); %Arbitrarily large pause so weird 
+pause(3000); %Arbitrarily large pause so weird 
               %buffer stuff doesn't happen.
